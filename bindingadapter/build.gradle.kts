9c133687e5ea1c6855d620d68045ee43dd828138
hongjb <hongjb@qq.com>
2024-11-11 11:25:08 +0800

    ...

--- bindingadapter/build.gradle.kts
+++ bindingadapter/build.gradle.kts
@@ -37,10 +37,7 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.material:material:1.12.0")
 }
